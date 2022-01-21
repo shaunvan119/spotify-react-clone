92b2d182361a20209f7d1053647ded187b4c96f6
shaunvan119 <shaunvan@live.com.au>
2022-01-21 11:03:59 +1100

    styling and material ui

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import "./Sidebar.js";
+import "./Sidebar.css"
+import SidebarOption from './SidebarOption.js';
+import HomeIcon from "@material-ui/icons/Home";
+import SearchIcon from "@material-ui/icons/Search";
+import LibraryMusicIcon from "@material-ui/icons/LibraryMusic";
 
 function Sidebar() {
     return (
         <div className='sidebar'>
+        <img 
+        className="sidebar__logo"
+        src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg" alt=""/>
         
-        <h1>I am the side bar</h1>
-     
-        </div>
-    )
+
+        <SidebarOption Icon={HomeIcon} title="Home"/>
+        <SidebarOption Icon={SearchIcon} title="Search"/>
+        <SidebarOption Icon={LibraryMusicIcon} title="Your Libary"/>
+        
+        <br/>
+        <strong className="sidebar__title">PLAYLIST</strong> 
+        <hr/>
+
+        <SidebarOption title="Rock"/>
+        <SidebarOption title="RnB"/>
+        <SidebarOption title="Alternative"/>
+    </div>
+    );
 }
 
 export default Sidebar
